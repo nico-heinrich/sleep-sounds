6929f3a98f4eba7ad10605b09816864634fb3b8f
DevExord <dev@exord.de>
2025-11-20 20:25:53 +0100

    make repo clean

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/options" style={{ marginTop: 20, fontSize: 18 }}>
+        Go to Options
+      </Link>
+    </View>
+  );
+}
---
+++ app/options/index.tsx
@@ -0,0 +1,99 @@
+import { Text, ScrollView } from "react-native";
+import { Link } from "expo-router";
+
+export default function Options() {
+  return (
+    <ScrollView>
+      <Text style={{ marginTop: 100 }}>This is the options screen</Text>
+      <Link href="/" style={{ marginTop: 20, fontSize: 18 }}>
+        Go to Main
+      </Link>
+      <Text style={{ marginHorizontal: 20, fontSize: 14 }}>
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+      </Text>
+    </ScrollView>
+  );
+}
