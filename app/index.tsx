a04bea7ff75fd2c0aedaa6493f953b465e857f68
DevExord <dev@exord.de>
2025-11-22 16:34:59 +0100

    fix button

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import { Text, View } from "react-native";
 import { Link } from "expo-router";
+import { Image } from "expo-image";
 import Button from "../components/ui/Button";
 
 export default function Index() {
@@ -9,18 +10,45 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "black",
       }}
     >
       <Text>Lesss gooooo</Text>
-      <Button
-        variant="primary"
-        label="Press Me"
-        onPress={() => {
-          alert("Button Pressed!");
+      <View
+        style={{
+          flexDirection: "row",
+          gap: 10,
+          marginTop: 600,
         }}
       >
-        <Text>🔥</Text>
-      </Button>
+        <Button small label="mini" onPress={() => {}}>
+          <Image
+            source="https://cdn-icons-png.flaticon.com/512/1828/1828817.png"
+            style={{ height: "100%", aspectRatio: 1 }}
+          />
+        </Button>
+        <View>
+          <Button
+            label="Select"
+            onPress={() => {
+              alert("Button Pressed!");
+            }}
+          >
+            <Image
+              source="https://cdn-icons-png.flaticon.com/512/1828/1828817.png"
+              style={{ height: "100%", aspectRatio: 1 }}
+            />
+          </Button>
+        </View>
+        <View>
+          <Button
+            label="Secondary Button"
+            onPress={() => {
+              alert("Secondary Button Pressed!");
+            }}
+          />
+        </View>
+      </View>
       <Link href="/options" style={{ marginTop: 20, fontSize: 18 }}>
         Go to Options
       </Link>
--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,45 +1,82 @@
-import { Pressable, Text, View } from "react-native";
+import { Pressable, Text, Animated } from "react-native";
+import { BlurView } from "expo-blur";
+import { useRef } from "react";
 
 export default function Button({
-  variant = "primary",
+  small = false,
   label,
   children,
   onPress,
 }: {
-  variant?: "primary" | "secondary";
-  label: string;
+  small?: boolean;
+  label?: string;
+  icon?: React.ReactNode;
   children?: React.ReactNode;
   onPress: () => void;
 }) {
-  const backgroundColor = variant === "primary" ? "#007bff" : "#6c757d";
-  const textColor = "#ffffff";
+  const height = small ? 42 : 68;
+  const padding = small ? 6 : 10;
+  const fontSize = small ? 14 : 16;
+
+  const scale = useRef(new Animated.Value(1)).current;
+
+  const handlePressIn = () => {
+    Animated.spring(scale, {
+      toValue: 0.95,
+      useNativeDriver: true,
+      speed: 20,
+      bounciness: 5,
+    }).start();
+  };
+
+  const handlePressOut = () => {
+    Animated.spring(scale, {
+      toValue: 1,
+      useNativeDriver: true,
+      speed: 20,
+      bounciness: 5,
+    }).start();
+  };
 
   return (
-    <Pressable onPress={onPress}>
-      <View
-        style={{
-          backgroundColor,
-          padding: 12,
-          borderRadius: 9999,
-          flexDirection: "row",
-          alignItems: "center",
-        }}
+    <Animated.View
+      style={{
+        transform: [{ scale }],
+        height,
+      }}
+    >
+      <Pressable
+        onPress={onPress}
+        onPressIn={handlePressIn}
+        onPressOut={handlePressOut}
+        style={{ height: "100%" }}
       >
-        {children}
-        {label && (
-          <Text
-            style={{
-              color: textColor,
-              textAlign: "center",
-              fontSize: 16,
-              fontWeight: "600",
-              paddingHorizontal: 8,
-            }}
-          >
-            {label}
-          </Text>
-        )}
-      </View>
-    </Pressable>
+        <BlurView
+          style={{
+            height: "100%",
+            padding,
+            borderRadius: 9999,
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          {children}
+          {label && (
+            <Text
+              style={{
+                color: "white",
+                textAlign: "center",
+                fontSize,
+                fontWeight: "600",
+                paddingStart: children ? padding * 1.25 : padding * 2,
+                paddingEnd: padding * 2,
+              }}
+            >
+              {label}
+            </Text>
+          )}
+        </BlurView>
+      </Pressable>
+    </Animated.View>
   );
 }
