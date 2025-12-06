b4b6c511ad7183a4c6510551a626e9397a263abf
DevExord <dev@exord.de>
2025-12-06 22:57:36 +0100

    add heading

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,20 @@
 import { VideoView, useVideoPlayer } from "expo-video";
 import { useEffect } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import Animated, { FadeIn, FadeOut } from "react-native-reanimated";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import HomeControls from "../components/HomeControls";
 import { useSound } from "../contexts/SoundContext";
 import { sets } from "../data/sets";
 
 export default function Index() {
-  const { currentSoundId, isPlaying, playSound, stopSound } = useSound();
+  const {
+    currentSoundId,
+    isPlaying,
+    playSound,
+    stopSound,
+    shouldAnimateControls,
+  } = useSound();
 
   // Default to first set if no sound is selected
   const activeSetId = currentSoundId || "01";
@@ -47,6 +54,17 @@ export default function Index() {
       {/* Vignette Overlay */}
       <View style={styles.vignette} />
 
+      {/* Heading - shown when not playing */}
+      {!isPlaying && (
+        <Animated.View
+          entering={shouldAnimateControls ? FadeIn.duration(600) : undefined}
+          exiting={shouldAnimateControls ? FadeOut.duration(400) : undefined}
+          style={styles.headingContainer}
+        >
+          <Text style={styles.heading}>time to sleep well</Text>
+        </Animated.View>
+      )}
+
       {/* Home Controls */}
       <HomeControls isPlaying={isPlaying} onPlayToggle={handlePlayToggle} />
     </View>
@@ -73,5 +91,24 @@ const styles = StyleSheet.create({
     bottom: 0,
     boxShadow: "inset 0 0 200px rgba(0,0,0,0.9)",
     pointerEvents: "none",
+  },
+  headingContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    paddingInline: 24,
+    justifyContent: "center",
+    alignItems: "center",
+    pointerEvents: "none",
+  },
+  heading: {
+    fontSize: 50,
+    fontFamily: "Satoshi",
+    fontWeight: "700",
+    color: "white",
+    textAlign: "center",
+    paddingHorizontal: 40,
   },
 });
